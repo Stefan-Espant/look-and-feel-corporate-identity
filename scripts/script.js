526bbd78c5cc119c302f840c5ad24be31b7e71a1
Stefan Espant <info@espant.com>
2022-11-24 16:09:29 +0100

    v2.0
    Dark theme functie toegevoegd en kleuren hierop ingesteld.

--- scripts/script.js
+++ scripts/script.js
@@ -46,4 +46,31 @@ function sendEmotion() {
         </div>
     `
     element.innerHTML = html
+};
+
+// De toggle settings for switching to dark mode 
+
+const btn = document.querySelector(".btn-colorscheme");
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme == "light") {
+  document.body.classList.toggle("dark-theme");
+} else if (currentTheme == "light") {
+  document.body.classList.toggle("light-theme");
 }
+
+btn.addEventListener("click", function () {
+  if (prefersDarkScheme.matches) {
+    document.body.classList.toggle("light-theme");
+    var theme = document.body.classList.contains("light-theme")
+      ? "light"
+      : "dark";
+  } else {
+    document.body.classList.toggle("dark-theme");
+    var theme = document.body.classList.contains("dark-theme")
+      ? "dark"
+      : "light";
+  }
+  localStorage.setItem("theme", theme);
+});
